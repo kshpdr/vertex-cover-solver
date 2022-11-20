7d183a990b55c6394c73fb4ef7788a43928990c9
deniskoshelev <koshelev@campus.tu-berlin.de>
2022-11-20 23:23:19 +0000

    Format Vertex.java

--- task-2/src/Vertex.java
+++ task-2/src/Vertex.java
@@ -2,50 +2,37 @@ public class Vertex implements Comparable<Vertex>{
     int label;
     int degree;
     int dist = 0;
+    
     Vertex(Integer label) {
         this.label = label;
     }
 
     // equals and hashCode
-
     @Override
     public boolean equals(Object o) {
-
         // If the object is compared with itself then return true
         if (o == this) {
             return true;
         }
-
-
         if (!(o instanceof Vertex)) {
             return false;
         }
-
-
-
         return this.label == ((Vertex) o).label;
     }
 
     @Override
     public int hashCode() {
-
         return this.label;
     }
 
     @Override
     public String toString() {
-
         //sb.append("Vertices: ").append("\n");
-
         return "Vertex " + this.label +" degree " + this.degree;
     }
 
-
     @Override
     public int compareTo(Vertex o) {
-
         return Integer.compare(this.degree,  o.degree);
     }
-
-
 }
