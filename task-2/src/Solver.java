46b00e116edb63958dd805e39f87948abe371bdc
Julio Cesar Perez Duran <juliop1996@campus.tu-berlin.de>
2022-11-21 01:19:18 +0100

    Revert "Update Solver.java"
    This reverts commit e06798f6285dbad307a4f8b9fd36ec3f74453b9e.

--- task-2/src/Solver.java
+++ task-2/src/Solver.java
@@ -4,37 +4,57 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Solver {
-
     public static void main(String[] args) throws IOException {
-        // Use buffer reader for stdin since the input does not require validation, and it is much faster than scanner
-        BufferedReader bi = new BufferedReader(new InputStreamReader(System.in));
-        //Storing edges to call the graph constructor afterwards
-        HashSet<String[]> edges = new HashSet<>();
-        String line;
 
+        // Use buffer reader for stdin since the input does not require validation, and it is much faster than scanner
+
+        BufferedReader bi = new BufferedReader(new InputStreamReader(System.in));
+
+
+        //Storing edges to call the graph constructor afterwards
+
+
+        HashSet<String[]> edges = new HashSet<>();
+
+        String line;
         while (((line = bi.readLine()) !=null)){
+
             // Ignore lines starting with # and empty lines
+
             if (!line.contains("#") && !line.isEmpty()){
+
                 //Striping line from whitespaces
+
                 String[] nodes = line.split("\\s+");
                 edges.add(nodes);
             }
+
+
         }
 
         // Instantiate graph
         Graph graph = new Graph(edges);
+
         // Call method with the clique lower bound
+
+
         SolverResult result = vc(graph, graph.getMaxLowerBound());
+
         // Putting it all together in one String to only use one I/O operation
+
         StringBuilder sb = new StringBuilder();
+
+
 
         if (!result.resultsList.isEmpty()){
             for (String s : result.resultsList) {
+
                 sb.append(s).append("\n");
             }
         }
 
         sb.append("#recursive steps: ").append(result.recursiveSteps).append("\n");
+
         String resultStr = sb.toString();
         System.out.print(resultStr);
     }
@@ -46,38 +66,61 @@ public class Solver {
             solverResult.setEmptyResultsList();
             return solverResult;
         }
+
         solverResult.increaseRecursiveSteps();
+
+
         // Get vertex with the highest degree
         Vertex v = graph.getNextNode();
+//
         HashSet<Vertex> eliminatedNeighbors = graph.removeVertex(v);
 
+
+
+
         SolverResult s = vc_branch(graph,k-1, solverResult);
+
+
         graph.putVertexBack(v,eliminatedNeighbors);
+
+
 
         if (s.resultsList != null) {
             s.addVertexToResult(graph.getVertexMapping(v));
             return s;
         }
 
+
         //Eliminating the neighbors of the vertex with the highest degree and storing the neighbors of the neighbors with a hashmap
+
         HashMap<Vertex,HashSet<Vertex>> eliminatedNeighborsMap= graph.removeSetofVertices(eliminatedNeighbors);
+
+
         //Branching with the neighbors
         s = vc_branch(graph,k-eliminatedNeighbors.size(), solverResult);
+
         //Putting back the eliminated vertices
+
         graph.putManyVerticesBack(eliminatedNeighborsMap);
+
+
         if (s.resultsList != null) {
             solverResult.addMultipleVertexToResult(graph.getMultipleMappings(eliminatedNeighbors));
             return solverResult;
         }
 
+
         return new SolverResult();
+
     }
 
 
     // main function which increases the cover vertex size k every iteration
+
     public static SolverResult vc(Graph graph,int lowerBound){
         SolverResult  s = new SolverResult();
         int k = lowerBound;
+
         while ((vc_branch(graph,k++,s)).resultsList==null){}
         return s;
     }
@@ -85,7 +128,7 @@ public class Solver {
     static class SolverResult{
         private LinkedList<String> resultsList = null;
         private int recursiveSteps;
-        
+
         SolverResult() {
 
         }
@@ -95,7 +138,9 @@ public class Solver {
         }
 
         private void addMultipleVertexToResult(String[] verticesToAdd){
+
             Collections.addAll(this.resultsList, verticesToAdd);
+
         }
 
         private void increaseRecursiveSteps(){
@@ -105,5 +150,19 @@ public class Solver {
         private void setEmptyResultsList(){
             this.resultsList = new LinkedList<>();
         }
+
     }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
