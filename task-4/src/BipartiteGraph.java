ccad03fa3d7e76c02c7915e1558f920a0efa3338
juliop1980 <14-10820@usb.ve>
2022-12-21 19:44:37 +0100

    Basis code für die Aufgabe 4

---
+++ task-4/src/BipartiteGraph.java
@@ -0,0 +1,176 @@
+import java.io.IOException;
+import java.util.*;
+
+public class BipartiteGraph {
+    public Vertex nilVertex = new Vertex("nil", -1);
+    static final int INF = Integer.MAX_VALUE;
+
+    public ArrayList<Vertex> left = new ArrayList<>();
+    public ArrayList<Vertex> right = new ArrayList<>();
+    public HashMap<Vertex,HashSet<Vertex>> adjacentMap = new HashMap<>();
+
+    HashMap<Vertex, Vertex> pairLeft, pairRight;
+    HashMap<Vertex, Integer> dist;
+
+    public BipartiteGraph(Graph graph) {
+        nilVertex.dist = INF;
+        int indexCounter = 0;
+        for (Vertex vertex : graph.getVertices()) {
+            Vertex v = new Vertex(vertex.name, indexCounter++);
+            v.originalId = vertex.id;
+            left.add(v);
+
+            Vertex u = new Vertex(vertex.name, indexCounter++);
+            u.originalId = vertex.id;
+            right.add(u);
+        }
+
+        HashMap<Vertex,HashSet<Vertex>> map = graph.getAdjVertices();
+        for (Vertex firstVertex : map.keySet()){
+            HashSet<Vertex> neighbors = map.get(firstVertex);
+            if (neighbors == null) continue;
+            for (Vertex secondVertex : neighbors){
+                // Skip reverse edges: (0,1) ok, but not (1,0) again => avoid duplicates
+                if (firstVertex.id > secondVertex.id) continue;
+
+                Vertex leftFirst = null;
+                Vertex leftSecond = null;
+                for (Vertex vertex : left){
+                    if (vertex.originalId == firstVertex.id){
+                        leftFirst = vertex;
+                    }
+                    else if (vertex.originalId == secondVertex.id){
+                        leftSecond = vertex;
+                    }
+                }
+
+                Vertex rightFirst = null;
+                Vertex rightSecond = null;
+                for (Vertex vertex : right){
+                    if (vertex.originalId == firstVertex.id){
+                        rightFirst = vertex;
+                    }
+                    else if (vertex.originalId == secondVertex.id){
+                        rightSecond = vertex;
+                    }
+                }
+
+                if (leftFirst != null && leftSecond != null && rightFirst != null && rightSecond != null){
+                
+                    // leftFirst -> rightSecond
+                    HashSet<Vertex> neighborSet = adjacentMap.computeIfAbsent(leftFirst, k -> new HashSet<>());
+                    neighborSet.add(rightSecond);
+                    // leftFirst <- rightSecond
+                    neighborSet = adjacentMap.computeIfAbsent(rightSecond, k -> new HashSet<>());
+                    neighborSet.add(leftFirst);
+                    
+                    // leftSecond -> rightFirst
+                    neighborSet = adjacentMap.computeIfAbsent(leftSecond, k -> new HashSet<>());
+                    neighborSet.add(rightFirst);
+                    // leftSecond <- rightFirst
+                    neighborSet = adjacentMap.computeIfAbsent(rightFirst, k -> new HashSet<>());
+                    neighborSet.add(leftSecond);
+                }
+            }
+        }
+    }
+
+    // horcropft-carp algorithm for maximum matching in bipartite graphs
+    public int findMaximumMatchingSize() {
+        int result = 0;
+
+        pairLeft = new HashMap<>(left.size() + 1);
+        pairRight = new HashMap<>(right.size() + 1);
+        dist = new HashMap<>(left.size() + 1);
+
+        for (Vertex vertex : left) {
+            pairLeft.put(vertex, nilVertex);
+            dist.put(vertex, 0);
+        }
+
+        for (Vertex vertex : right) {
+            pairRight.put(vertex, nilVertex);
+        }
+
+        // Keep updating the result while
+        // there is an augmenting path.
+        while (bfs()) {
+            // Find a free vertex
+            for (Vertex vertex : pairLeft.keySet())
+                // If current vertex is free and there is
+                // an augmenting path from current vertex
+                if (pairLeft.get(vertex) == nilVertex && dfs(vertex))
+                    result++;
+        }
+
+        return result;
+    }
+
+    // returns true, when augmenting path exists
+    public boolean bfs() {
+        Queue<Vertex> queue = new LinkedList<>();
+        for (Vertex vertex : pairLeft.keySet()) {
+            if (pairLeft.get(vertex) == nilVertex) {
+                // u is not matched
+                dist.replace(vertex, 0);
+                queue.add(vertex);
+            } else
+                dist.replace(vertex, INF);
+        }
+        dist.put(nilVertex, INF);
+        while (!queue.isEmpty()) {
+            Vertex vertex = queue.poll();
+            if (dist.get(vertex) < dist.get(nilVertex)) {
+                HashSet<Vertex> neighbors = adjacentMap.get(vertex);
+                if (neighbors != null){
+                    for (Vertex neighbor : neighbors){
+                        if (dist.get(pairRight.get(neighbor)) == INF) {
+                            dist.replace(pairRight.get(neighbor), dist.get(vertex) + 1);
+                            queue.add(pairRight.get(neighbor));
+                        }
+                    }
+                }
+            }
+        }
+        return (dist.get(nilVertex) != INF);
+    }
+
+    public boolean dfs(Vertex vertex) {
+        if (vertex != nilVertex) {
+            HashSet<Vertex> neighbors = adjacentMap.get(vertex);
+            if (neighbors != null){
+                for (Vertex neighbor : neighbors){
+                    if (dist.get(pairRight.get(neighbor)) == dist.get(vertex) + 1) {
+                        if (dfs(pairRight.get(neighbor))) {
+                            pairRight.replace(neighbor, vertex);
+                            pairLeft.replace(vertex, neighbor);
+                            return true;
+                        }
+                    }
+                }
+            }
+            dist.replace(vertex, INF);
+            return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) throws IOException {
+//        InputParser inputParser = new InputParser();
+
+//        List<String> stringEdges = inputParser.parseEdges();
+
+//        HashSet<String[]> edges = new HashSet<>();
+//        for (String stringEdge : stringEdges) {
+//
+//            String[] nodes = stringEdge.split("\\s+");
+//            edges.add(nodes);
+//
+//        }
+
+//        Graph graph = new Graph(edges);
+//        BipartiteGraph bipartiteGraph = new BipartiteGraph(graph);
+//        System.out.println(bipartiteGraph.findMaximumMatchingSize());
+    }
+}
+
---
+++ task-4/src/ReductionRules.java
@@ -0,0 +1,156 @@
+import java.util.*;
+
+public class ReductionRules {
+    private final boolean oneDegreeRule;
+    private final boolean twoDegreeRule;
+    private final boolean dominationRule;
+    private final boolean anyRule;
+    private final HashMap<String,ArrayList<String>> mergeMap;
+    private final LinkedList<String> mergeOrder;
+
+    public ReductionRules(boolean oneDegreeRule, boolean twoDegreeRule, boolean dominationRule){
+        this.oneDegreeRule = oneDegreeRule;
+        this.twoDegreeRule = twoDegreeRule;
+        this.dominationRule = dominationRule;
+        this.anyRule = oneDegreeRule || twoDegreeRule || dominationRule;
+        this.mergeMap = new HashMap<>();
+        this.mergeOrder = new LinkedList<>();
+    }
+
+    public LinkedList<String> applyReductionRules(HashSet<String[]> edges) {
+        if (!anyRule) return new LinkedList<>();
+
+        // Build Adjacency-Map (to extract node-degree) from HashSet (of Edges)
+        HashMap<String, HashSet<String>> adjMap = new HashMap<>();
+        for (String[] edge : edges) {
+            for (int i = 0; i < 2; i++) {
+                String u = edge[i % 2];
+                String v = edge[(i + 1) % 2];
+                HashSet<String> neighbors = adjMap.computeIfAbsent(u, k -> new HashSet<>());
+                neighbors.add(v);
+
+                neighbors = adjMap.computeIfAbsent(v, k -> new HashSet<>());
+                neighbors.add(u);
+            }
+        }
+
+        // Instatiate solver result (for partial solution obtained from applying
+        // reduction rules multiple times)
+        LinkedList<String> result = new LinkedList<>();
+
+        // Until no more reduction possible!
+        while (true) {
+            boolean reduced = false;
+            // Loop over all vertices
+            for (String v : new LinkedList<>(adjMap.keySet())) {
+                // Try to find a vertex v with degree = 1
+                HashSet<String> neighbors = adjMap.get(v);
+                if (neighbors == null) continue;
+                if (oneDegreeRule && neighbors.size() == 1) {
+                    for (String singleNeighbor : neighbors) {
+                        removeVertex(adjMap, singleNeighbor);
+                        reduced = true;
+                        result.add(singleNeighbor);
+                    }
+                }
+                else if (twoDegreeRule && neighbors.size() == 2){
+                    ArrayList<String> arr = new ArrayList<>(neighbors);
+                    String u = arr.get(0);
+                    String w = arr.get(1);
+                    arr.add(v);
+                    if (adjMap.get(u).contains(w)){
+                        result.add(u);
+                        result.add(w);
+                        removeVertex(adjMap, u);
+                        removeVertex(adjMap, w);
+                        removeVertex(adjMap, v);
+                    }
+                    else {
+                        arr.add(v);
+                        String x = u + " - "+ v + " - " + w;
+                        //System.out.println("#Degree-2 Rule: Case-2 ==> "+x);
+                        //System.out.println("#Before: "+adjMap);
+                        mergeMap.put(x,arr);
+                        mergeOrder.add(x);
+                        HashSet<String> newNeighbors = new HashSet<>();
+                        newNeighbors.addAll(adjMap.get(u));
+                        newNeighbors.addAll(adjMap.get(w));
+                        arr.forEach(newNeighbors::remove);
+                        for (String n : newNeighbors){
+                            String[] edge = {x,n};
+                            edges.add(edge);
+                        }
+                        removeVertex(adjMap, u);
+                        removeVertex(adjMap, w);
+                        removeVertex(adjMap, v);
+                        addVertex(adjMap, x, newNeighbors);
+                        //System.out.println("#After: "+adjMap);
+                        reduced = true;
+                    }
+                }
+                if (dominationRule){
+                    boolean delete = false;
+                    for (String n : neighbors){
+                        HashSet<String> neighbors2 = adjMap.get(n);
+                        neighbors2.remove(v);
+                        if (neighbors.size() >= neighbors2.size() && neighbors.containsAll(neighbors2)){
+                            neighbors2.add(v);
+                            delete = true;
+                            reduced = true;
+                            break;
+                        }
+                        else neighbors2.add(v);
+                    }
+                    if (delete){
+                        removeVertex(adjMap, v);
+                        result.add(v);
+                    }
+                }
+            }
+            if (!reduced) break;
+        }
+
+        // Loop through initial HashSet of edges and delete (... those which are not
+        // used anymore)
+        edges.removeIf(edge -> !adjMap.containsKey(edge[0]) || !adjMap.get(edge[0]).contains(edge[1]));
+
+        // Return partial result obtained from reduction rules
+        return result;
+    }
+
+    void undoMerge(LinkedList<String> solution){
+        //System.out.println("#Result: "+solution);
+        while (mergeOrder.size() > 0) {
+            String x = mergeOrder.removeLast();
+            ArrayList<String> vertices = mergeMap.get(x);
+            int index = solution.indexOf(x);
+            if (index != -1){
+                solution.remove(index);
+                solution.add(vertices.get(0));  // Vertex u
+                solution.add(vertices.get(1));  // Vertex w
+                //System.out.println("#Undo: "+x+" => "+vertices.subList(0, 1));
+            }
+            else {
+                solution.add(vertices.get(2));  // Vertex v
+                //System.out.println("#Undo: "+x+" => "+vertices.get(2));
+            }
+        }
+    }
+
+    private void removeVertex(HashMap<String,HashSet<String>> adjMap, String vertex){
+        HashSet<String> neighbors = adjMap.remove(vertex);
+        if (neighbors != null){
+            for (String neighbor : neighbors){
+                HashSet<String> nextNeighbors = adjMap.get(neighbor);
+                if (nextNeighbors != null) nextNeighbors.remove(vertex);
+            }
+        }
+    }
+
+    private void addVertex(HashMap<String, HashSet<String>> adjMap, String v, HashSet<String> neighbors){
+        adjMap.put(v,neighbors);
+        for (String n : neighbors){
+            adjMap.get(n).add(v);
+        }
+    }
+}
