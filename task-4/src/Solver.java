6440b3c1816a2a5fa15b864f1edad8bc7c2f9b9f
Julian Fechner <fechner-julian@campus.tu-berlin.de>
2022-12-24 00:30:45 +0100

    rewrite complete code ... minimal solution

--- task-4/src/Solver.java
+++ task-4/src/Solver.java
@@ -1,75 +1,78 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.*;
 
 public class Solver {
-    public static boolean oneDegreeRulePre =false;
-    public static boolean twoDegreeRulePre = false;
-    public static boolean dominationRulePre = false;
+    public static HashMap<String,HashSet<String>> parseInput(){
+        // Store graph as adjacency-list
+        HashMap<String,HashSet<String>> adjMap = new HashMap<>();
 
-    public static boolean lpBoundBeginning  = false;
-    public static boolean cliqueBoundBeginning =false;
-    public static boolean unconfinedRuleBeginning = false;
-    public static boolean highDegreeRuleBeginning = false;
-    public static boolean lpReductionBeginning = false;
+        // Parse input graph
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()){
+            // Line by line
+            String line = scanner.nextLine();
+            // Skip comments and empty lines
+            if (line.startsWith("#") || line.length() == 0) continue;
+            // Parse edge
+            String[] edge = line.split(" ");
+            String u = edge[0];
+            String v = edge[1];
 
-    public static boolean cliqueBoundIteration= false;
-    public static boolean lpBoundIteration= false;
-    public static boolean dominationRuleIteration = false;
-    public static boolean unconfinedRuleIteration = false;
-    public static boolean highDegreeRuleIteration = false;
-    public static boolean oneDegreeRuleIteration = false;
-    public static boolean twoDegreeRuleIteration = false;
-    public static StringBuilder sb = new StringBuilder();
-
-    public static int recursiveSteps = 0;
-
-    // main function which increases the cover vertex size k every iteration
-    public static void vc_greedy_heuristic(Graph graph) {
-        while (!graph.isEmpty()){
-            // Get vertex with the highest degree
-            Vertex v = graph.getNextNode();
-            // Remove it from graph
-            graph.removeVertexVoid(v);
-            // Add it to result
-            sb.append(v.name).append("\n");
-            recursiveSteps++;
+            // Save (u,v) in adjacency-list
+            HashSet<String> neighbors = adjMap.get(u);
+            if (neighbors == null){
+                neighbors = new HashSet<>();
+                adjMap.put(u,neighbors);
+            }
+            neighbors.add(v);
+            // Save (v,u) in adjacency-list
+            neighbors = adjMap.get(v);
+            if (neighbors == null){
+                neighbors = new HashSet<>();
+                adjMap.put(v,neighbors);
+            }
+            neighbors.add(u);
         }
+        scanner.close();
+
+        return adjMap;
     }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader bi = new BufferedReader(new InputStreamReader(System.in));
-
-        // Storing edges to call the graph constructor afterwards
-        HashSet<String[]> edges = new HashSet<>();
-
-        String line;
-        while (((line = bi.readLine()) != null)) {
-            if (!line.contains("#") && !line.isEmpty()) {
-                String[] nodes = line.split("\\s+");
-//                if(nodes.length==1){
-//                    System.exit(0);
-//                }
-                edges.add(nodes);
-
+    public static void greedy_vertex_cover_heuristic(HashMap<String,HashSet<String>> adjMap){
+        StringBuilder sb = new StringBuilder();
+        int lastMaxDegree = -2;
+        // Repeat until graph completely covered ...
+        while (!adjMap.isEmpty()){
+            // Find max degree vertex
+            String maxVertex = null;
+            int maxDegree = -1;
+            for (Map.Entry<String,HashSet<String>> entry : adjMap.entrySet()){
+                String v = entry.getKey();
+                int degree = entry.getValue().size();
+                if (maxVertex == null || maxDegree < degree){
+                    maxVertex = v;
+                    maxDegree = degree;
+                    if (lastMaxDegree == maxDegree) break;
+                }
+            }
+            lastMaxDegree = maxDegree;
+            // Add it to solution
+            sb.append(maxVertex+"\n");
+            // Remove it from graph
+            HashSet<String> neighbors = adjMap.remove(maxVertex);
+            // Remove it from its neighbors
+            for (String neighbor : neighbors){
+                HashSet<String> nextNeighbors = adjMap.get(neighbor);
+                nextNeighbors.remove(maxVertex);
+                // Delete 0-degree nodes
+                if (nextNeighbors.isEmpty()){
+                    adjMap.remove(neighbor);
+                }
             }
         }
-
-        long start = System.currentTimeMillis();
-    
-        // Instantiate graph
-        Graph graph = new Graph(edges);
-
-        vc_greedy_heuristic(graph);
-
-        sb.append("#recursive steps: ").append(recursiveSteps).append("\n");
-
-        String resultStr = sb.toString();
-        System.out.print(resultStr);
-
-        long end = System.currentTimeMillis();
-        float sec = (end - start) / 1000F;
-        System.out.println("#time: " + sec);
+        // Print result
+        System.out.print(sb.toString());
+    }
+    public static void main(String[] args){
+        HashMap<String,HashSet<String>> adjMap = parseInput();
+        greedy_vertex_cover_heuristic(adjMap);
     }
 }
