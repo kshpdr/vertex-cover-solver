00318e32c6f1e88c141df4e599e84c52042f8028
Julian Richard Fechner <j.fechner@campus.tu-berlin.de>
2022-12-30 12:41:30 +0000

    added MEDIAN (too-large error)

--- task-4/vc-data-students/stats.sh
+++ task-4/vc-data-students/stats.sh
@@ -6,12 +6,12 @@ if [ -z "$csv_file" ] ; then
 	exit 1
 fi
 
-tests_total=266
+tests_total=265
 tests_solved=$(grep -P "(OK;OK|1;OK;;)" $csv_file | wc -l)
 tests_failed=$(grep "Not a VC" $csv_file | wc -l)
 tests_timeout=$(grep -Po "^[^;]*;[^;]*;[^;]*;[^;]*;[^;]*;[^;]*;0;" $csv_file | wc -l)
 too_large=$(grep -P ">>INCORRECT" $csv_file)
-tests_larger=$(echo "$too_large" | wc -l)
+tests_larger=$(if [ -z "$too_large" ] ; then echo 0; else echo "$too_large" | wc -l; fi)
 
 tests_run=$(grep -P ".*.dimacs" $csv_file | wc -l)
 tests_skipped=$(( $tests_total - $tests_run))
@@ -29,6 +29,7 @@ avg=0
 stats_file=$(echo $csv_file | sed 's/results-/stats-/')
 echo "file;time;solver;diff;opt;pct;avg;max" > $stats_file
 
+pct_arr=( )
 for ((i=0;i<len;i++))
 do
 	sol=${solution_sizes[$i]}
@@ -40,11 +41,20 @@ do
 		pct="0"
 	fi
 	avg=$(echo $avg $pct $len | awk '{print $1 + $2 / $3}')
-	max=$(echo $max $pct | awk '{print $1 >= $2 ? $1 : $2}')
+	max=$(echo $max $pct | awk '{print ($1 >= $2 ? $1 : $2)}')
 	f=${files[$i]}
 	tm=${times[$i]}
+	pct_arr+=( $pct )
 	echo "$f;$tm;$sol;$diff;$opt;$pct;$avg;$max" >> $stats_file
 done
+
+for ((i=len;i<tests_total;i++))
+do
+	pct_arr+=( 0 )
+done
+
+inp=$(echo ${pct_arr[@]} | sed 's/,/./g' | sed 's/ /,/g')
+median=$(python3 -c "inp=[$inp];s=[float(p) for p in inp];s.sort();print(s[len(s)//2])")
 
 echo "====== STATS ======"
 echo "[*] Solved (OK;OK)          : $tests_solved / $tests_total" "("$(echo $tests_solved $tests_total | awk '{print int($1 / $2 * 100 + 0.5)}')"%)"
@@ -54,5 +64,6 @@ echo "[*] Too-Large (>>INCORRECT) : $tests_larger / $tests_total" "("$(echo $tes
 echo "[*] Skipped tests           : $tests_skipped / $tests_total" "("$(echo $tests_skipped $tests_total | awk '{print int($1 / $2 * 100 + 0.5)}')"%)"
 echo "[*] AVG Too-Large           : $avg %"
 echo "[*] MAX Too-Large           : $max %"
+echo "[*] MEDIAN Too-Large        : $median %"
 echo "====== STATS ======"
 echo "=> See details in stats file: $stats_file"
